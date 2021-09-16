8b903203cdd009545c6ffdd1843d0ed9978b922d
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2021-09-16 16:38:58 -0300

    Add new script

---
+++ scripts/add-preload-files.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+var path = require('path');
+
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      file = path.resolve(dir, file);
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        } else {
+          results.push(file);
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+};
+
+walk(
+  "./images",
+  function(err, results) {
+    fs.writeFile(
+      '_includes/preload.html',
+      results.map(filePath => {
+        const filename = filePath.split("images/")[1]
+        return `<link rel="prefetch" as="image"  href="images/${filename}">`
+      }).join("\n"),
+      function (err) {
+        if (err) return console.log(err);
+        console.log('Add preload tags');
+    });
+
+    fs.writeFile(
+      '_sass/settings/preload.scss',
+      results.map((filePath, index) => {
+        const filename = filePath.split("images/")[1]
+        return `#preload-${index} { background: url(images/${filename}); display: none; }`
+      }).join("\n"),
+      function (err) {
+        if (err) return console.log(err);
+        console.log('Add preload to css');
+    });
+  }
+)
