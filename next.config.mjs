6948fd7f76ef044dff69937530c8377c6fc7914b
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2024-06-13 19:02:12 -0300

    Release Next app

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const nextConfig = {
   output: "export",
+  images: { unoptimized: true },
   sassOptions: {
     includePaths: [path.join(__dirname, "public", "css", "*.scss")],
   },
