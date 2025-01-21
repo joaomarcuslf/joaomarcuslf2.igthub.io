1826e3258cbb633eca75747996eb6939df239fa7
João Marcus Fernandes <53450523+joaomarcuslf@users.noreply.github.com>
2025-01-21 11:28:48 -0300

    Fix weird email replace

--- site.ts
+++ site.ts
@@ -1,4 +1,4 @@
-contact@joaomarcuslf.comexport default {
+export default {
   "name": "@joaomarcuslf.com - Full-stack Software engineer",
   "description": "I'm Software Engineer since 2015 with great expertise in Front-end development. I like to teach a lot, and write some posts, you can check more here.",
   "author": "João Marcus de Lemos Fernandes",
@@ -7,7 +7,7 @@ contact@joaomarcuslf.comexport default {
   "keywords": "Developer, Software engineer, Front-end, Full-stack, React, Javascript, Flask, Python, Joaomarcuslf, Tech Mentor, Mentorship, Mentoria, Senior Developer, Web development",
   "abstract": "My presentation website as Software developer",
   "avatar": "https://avatars0.githubusercontent.com/u/53450523?s=400&u=ba2eb6e6c63bc38c54e7db04e5132c833a83169c&v=4",
-  "email": "jmarcusfernandes@gmail.com",
+  "email": "contact@joaomarcuslf.com",
   "linktr": "https://linktr.ee/joaomarcuslf",
   "url": "https://joaomarcuslf.com",
   "calendar": "https://calendly.com/joaomarcuslf",
